0b38bc61ae40587e2ba65c794d474806a094c295
Bancha Datrungpithak <banchad81@gmail.com>
2024-05-05 15:02:14 +0700

    disable model state checking

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using InvoiceAPI;
 using InvoiceAPI.DAL;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddScoped<InvoiceDAL>();
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
